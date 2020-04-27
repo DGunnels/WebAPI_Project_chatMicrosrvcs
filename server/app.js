c5b02a04a72a600bd1c766a49e33661b3dd1c7bf
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-04-27 17:32:01 -0600

    Server backend
    Just added the server backend to the github for Zach/Red

---
+++ server/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var cors = require('cors');
+var jwt = require('jsonwebtoken');
+
+
+var app = express();
+module.exports = app;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(passport.initialize());
+
+var router = express.Router();
+
+router.route('/')
+    .get(function (req, res) { })
+    .post(function (req, res) { })
+    .put(function (req, res) { })
+    .delete(function (req, res) { })
+    .all(function (req, res) { });
+
+router.route('/signin')
+    .get(function (req, res) { })
+    .post(function (req, res) { })
+    .put(function (req, res) { })
+    .delete(function (req, res) { })
+    .all(function (req, res) { });
+
+router.route('/signup')
+    .get(function (req, res) { })
+    .post(function (req, res) { })
+    .put(function (req, res) { })
+    .delete(function (req, res) { })
+    .all(function (req, res) { });
+
+router.route('/home')
+    .get(function (req, res) { })
+    .post(function (req, res) { })
+    .put(function (req, res) { })
+    .delete(function (req, res) { })
+    .all(function (req, res) { });
+
+app.use('/', router);
+app.listen(process.env.PORT || 8080);
